e2b6a657b7228593e6d6023fc10e3ceb7d4b8a9c
Hummel <Steven.Hummel@deq.virginia.gov>
2020-12-03 12:04:35 -0500

    added bug function

--- CEDS_data/Salinity_regime_funs.R
+++ CEDS_data/Salinity_regime_funs.R
@@ -96,6 +96,46 @@ NCCA_Chla_fun=function(x){
     x > 20 ~"Poor"
   )}
 
+# dissolved inorganic N
+DIN_fun=function(x){
+  
+case_when(
+is.numeric(x)==FALSE ~ NA_character_, 
+x <=0.1 ~"Good",
+x >0.1 & x <0.5 ~"Fair",
+x >=0.5  ~"Poor",
+          TRUE ~ NA_character_)
+}
+
+# orthophosphate
+DIP_fun=function(x){
+  
+  case_when(
+    is.numeric(x)==FALSE ~ NA_character_, 
+    x <=0.01 ~"Good",
+    x >0.01 & x <0.5 ~"Fair",
+    x >=0.05  ~"Poor",
+    TRUE ~ NA_character_)
+}
+
+
+Entero_fun=function(x){
+case_when(
+    is.numeric(x)==FALSE ~ NA_character_, 
+    x <=10 ~"Good",
+    x >10 & x <104 ~"Fair",
+    x >=104  ~"Poor",
+    TRUE ~ NA_character_)
+}
+
+Ecoli_fun=function(x){
+  case_when(
+    is.numeric(x)==FALSE ~ NA_character_, 
+    x <=24 ~"Good",
+    x >24 & x <235 ~"Fair",
+    x >=235  ~"Poor",
+    TRUE ~ NA_character_)
+}
 
 
 #------------------------------ DCLS Analytes -------------------------------------------------------------------------
@@ -110,7 +150,16 @@ NCCA_Chla_fun=function(x){
 # PERCENT SAND IN SEDIMENT ON A DRY WEIGHT BASIS =      82007
 # CARBON, ORGANIC, IN BED MATERIAL (GM/KG AS C)  =      00687
 # CHLOROPHYLL-A UG/L SPECTROPHOTOMETRIC ACID. METH.=    32211
- 
+
+# NITRATE NITROGEN, DISSOLVED (MG/L AS N)= 00618
+# NITRITE NITROGEN, DISSOLVED (MG/L AS N)= 00613
+# NITROGEN, AMMONIA, TOTAL DISSOLVED (MG/L AS N)= 00608
+# PHOSPHORUS, DISSOLVED ORTHOPHOSPHATE (MG/L AS P)=00671
+
+
+# ENTEROCOCCI- ME-MF N0/100ML = 31649
+# E. COLI - MTEC-MF N0/100ML  = 31648
+
 # Need to make more concise & clean up with purrr
 
 # param_names=c("Total_Nitrogen","Total_Phosphorus","Percent_Sand","TOC")  
@@ -118,46 +167,41 @@ NCCA_Chla_fun=function(x){
   
 DCLS_Analytes_fun=function(x){
   
- 
-TN= x %>%
-    group_by(Ana_Sam_Mrs_Container_Id_Desc,Ana_Sam_Fdt_Id,Fdt_Sta_Id,Fdt_Date_Time)%>%
-    filter(Pg_Storet_Code %in% c("49571","49570"))%>%
-    summarise(Total_Nitrogen=sum(Ana_Value))
+
+Summarized_Analytes= x %>% 
+  filter(Ana_Sam_Mrs_Container_Id_Desc %in% c("S1","S2","R")) %>% # get rid of blanks
+    group_by(Ana_Sam_Mrs_Container_Id_Desc,Fdt_Sta_Id,Fdt_Date_Time) %>% # one value for each replicate, station id and date_time,Unit
+       summarise(Total_Nitrogen=sum(Ana_Value[Pg_Storet_Code %in% c("49571","49570")]),
+              Total_Phosphorus=sum(Ana_Value[Pg_Storet_Code %in% c("49572","49567")]),
+              Dis_Inorg_N=sum(Ana_Value[Pg_Storet_Code %in% c("00618","00613","00608")]),
+              Dis_Inorg_P=sum(Ana_Value[Pg_Storet_Code %in% "00671"]),
+              TOC=sum(Ana_Value[Pg_Storet_Code %in% "00687"]),
+              Chlorophyll=sum(Ana_Value[Pg_Storet_Code %in% "32211"]),
+              Enterococci=sum(Ana_Value[Pg_Storet_Code %in% "31649"]),
+              Ecoli=sum(Ana_Value[Pg_Storet_Code %in% "31648"]),                    
+              Percent_Sand=sum(Ana_Value[Pg_Storet_Code %in% "82007"])) %>%
+
+  mutate(TOC_Quality=TOC_fun(TOC),
+         DIN_Quality=DIN_fun(Dis_Inorg_N),
+  DIP_Quality=DIP_fun(Dis_Inorg_P),
+  Chla_Quality=NCCA_Chla_fun(Chlorophyll),
+  Entero_Quality=Entero_fun(Enterococci),
+  Ecoli_Quality=Ecoli_fun(Ecoli),
+  Subtrate_Type=Percent_Sand_fun(Percent_Sand)) 
   
-TP= x %>%
-    group_by(Ana_Sam_Mrs_Container_Id_Desc,Ana_Sam_Fdt_Id,Fdt_Sta_Id,Fdt_Date_Time)%>%
-    filter(Pg_Storet_Code %in% c("49572","49567"))%>%
-    summarise(Total_Phosphorus=sum(Ana_Value))
   
-Sand= x %>%
-    group_by(Ana_Sam_Mrs_Container_Id_Desc,Fdt_Sta_Id,Fdt_Date_Time)%>%
-    filter(Pg_Storet_Code %in% "82007")%>%
-    summarise(Percent_Sand=max(Ana_Value))%>%
-    mutate(Subtrate_Type=Percent_Sand_fun(Percent_Sand))
+   
+return(Summarized_Analytes) 
   
-TOC= x %>%
-    group_by(Ana_Sam_Mrs_Container_Id_Desc,Fdt_Sta_Id,Fdt_Date_Time)%>%
-    filter(Pg_Storet_Code %in% "00687")%>%
-    summarise(TOC=max(Ana_Value))%>%
-    mutate(TOC_Quality=TOC_fun(TOC))
-  
-Chla= x %>%
-  group_by(Ana_Sam_Mrs_Container_Id_Desc,Fdt_Sta_Id,Fdt_Date_Time)%>%
-  filter(Pg_Storet_Code %in% "32211")%>%
-  summarise(Chlorophyll=max(Ana_Value)) %>%
-  mutate(Chlorophyll_Quality=NCCA_Chla_fun(Chlorophyll))
-  
-  #data.frame(plyr::join_all(list(TN,TP,Sand), type='left'))
-  Nutrients=left_join(TN,TP)
-  Sand_Toc=left_join(Sand,TOC)
-  
-  Total=left_join(Nutrients,Sand_Toc,by=c("Ana_Sam_Mrs_Container_Id_Desc","Fdt_Sta_Id","Fdt_Date_Time")) %>%
-  left_join(Chla)
-           
-  return(Total)          
+             
 }
 
-# ==================================================================
+
+
+# =================== Reading file fun ===============================================
+
+
+# Multiple excel sheets with multiple tabs. Gross
 
 
 #====================== Toxicity testing data=======================
@@ -193,9 +237,147 @@ readfilefun=function(folderpath){
   
 }
 
-#==================================== 
+#=================================================== 
+
+#--------------Don Toxicity Scoring------------------ 
+
+# -	No data available		
+# 0	No significant toxicity		
+# 1	Slight significant toxicity (corrected survivorship ≥ 75%),              1 spp		
+# 2	Moderate 1 spp (50< corrected survivorship < 75) or slight 2 or more spp mortality                                    		
+# 3	Severe 1 ssp (corrected survivorship ≤ 50) or moderate 2 or more spp toxicity		
 
 
+
+               #------------NCCA Toxicity Scoring-------------------
+
+#  (0)  	 If Ecological and Statistical significance both equal N, "Good"!			
+#  (1) 	   If only one equals N, "Fair".			
+#  (2) 	   If both equal Y, and control-corrected survivorship ≥ 50%, "Poor"!			
+#  (3)  	 If both equal Y, and control-corrected survivorship < 50%, "Very Poor"!			
+  
+
+
+#SIGNFICANTLY_DIFFERENT_Y_OR_N 
+#BIOLOGICALLY_SIGNIFICANT_80_PERCENT_Y_OR_N
+#CONTROL_CORRECTED_SURVIVAL
+
+
+NCCA_TOX_fun=function(x){
+  
+df= x %>%
+mutate(NCCA_TOX_Desc=
+case_when(
+  SIGNFICANTLY_DIFFERENT_Y_OR_N=="N" & BIOLOGICALLY_SIGNIFICANT_80_PERCENT_Y_OR_N=="N" ~ "Good",
+  SIGNFICANTLY_DIFFERENT_Y_OR_N=="Y" & BIOLOGICALLY_SIGNIFICANT_80_PERCENT_Y_OR_N=="N" ~ "Fair",
+  SIGNFICANTLY_DIFFERENT_Y_OR_N=="N" & BIOLOGICALLY_SIGNIFICANT_80_PERCENT_Y_OR_N=="Y" ~ "Fair",
+  SIGNFICANTLY_DIFFERENT_Y_OR_N=="Y" & BIOLOGICALLY_SIGNIFICANT_80_PERCENT_Y_OR_N=="Y" & CONTROL_CORRECTED_SURVIVAL >= 50 ~ "Poor",
+  SIGNFICANTLY_DIFFERENT_Y_OR_N=="Y" & BIOLOGICALLY_SIGNIFICANT_80_PERCENT_Y_OR_N=="Y" & CONTROL_CORRECTED_SURVIVAL < 50 ~ "Very Poor",
+  TRUE ~ NA_character_
+  ),NCCA_TOX_Score=
+  as.numeric(case_when(
+    NCCA_TOX_Desc=="Good"~ 0,
+    NCCA_TOX_Desc=="Fair"~ 1,
+    NCCA_TOX_Desc=="Poor"~ 2,
+    NCCA_TOX_Desc=="Very Poor"~ 3,
+    TRUE ~ NA_real_)))
+
+return(df)
+
+}
+
+
+#------------- California Toxicity categories -------------------------
+
+# control normalized =data from station/control data * 100
+# http://ftp.sccwrp.org/pub/download/DOCUMENTS/TechnicalReports/777_CASQO_TechnicalManual.pdf
+# https://oehha.ca.gov/media/downloads/ecotoxicology/general-info/marinetox3.pdf
+# Compiled from U.S. EPA 1994b, ASTM 2000e
+# http://ftp.sccwrp.org/pub/download/TOOLS/SQO/MLOE_Assessment_categories_table.pdf
+# Welch’s t-test
+#Nontoxic,
+#Low, 
+#Moderate, 
+#High
+Cal_TOX_fun=function(x){
+  
+df= x %>%
+mutate(Cal_TOX_Desc=case_when(
+  MEAN_SURVIVAL_PERCENT >80 ~ "Nontoxic",
+  CONTROL_CORRECTED_SURVIVAL >75 & SIGNFICANTLY_DIFFERENT_Y_OR_N =="N" ~"Nontoxic",
+  CONTROL_CORRECTED_SURVIVAL >75 & SIGNFICANTLY_DIFFERENT_Y_OR_N =="Y" ~"Low",
+  CONTROL_CORRECTED_SURVIVAL <75 & CONTROL_CORRECTED_SURVIVAL >50 & SIGNFICANTLY_DIFFERENT_Y_OR_N =="Y" ~"Moderate",
+  CONTROL_CORRECTED_SURVIVAL <50 ~"High"))
+
+return(df)
+
+}
+  
+ 
+#============================= B-IBI ===================================================
+
+
+#  Don's bug scores and weighted average
+#  notice that EMAP and MAMBI weights are always 1. 
+#  Ches bay and MAIA weights change based on basin
+
+bug_scores_fun=function(x,Basin,CB,MAIA,EMAP,MAMBI){
+
+  Basin=enquo(Basin)
+  CB=enquo(CB)  
+  MAIA=enquo(MAIA)
+  EMAP=enquo(EMAP)
+  MAMBI=enquo(MAMBI)
+  
+  x %>% 
+  mutate(
+  CB_WOE=
+  case_when(
+   is.numeric(!!CB)==FALSE ~ NA_real_, 
+    !!CB >= 2.95 ~0,
+    !!CB < 2.95 & !!CB >= 2.65 ~1,
+    !!CB < 2.65 & !!CB >= 2.05 ~2,
+    !!CB < 2.05 ~3
+  ),
+  MAIA_WOE=case_when(
+    is.numeric(!!MAIA)==FALSE ~ NA_real_, 
+    !!MAIA >= 2.95 ~0,
+    !!MAIA < 2.95 & !!MAIA >= 2.65 ~1,
+    !!MAIA < 2.65 & !!MAIA >= 2.05 ~2,
+    !!MAIA < 2.05 ~3
+  ),
+  EMAP_WOE=case_when( 
+    is.numeric(!!EMAP)==FALSE ~ NA_real_, 
+    !!EMAP > 0.1 ~ 0,
+    !!EMAP < 0.1 & !!EMAP >= -0.1 ~ 1,
+    !!EMAP < -0.1 & !!EMAP >= -0.996 ~ 2,
+    !!EMAP< -0.996 ~3
+  ),
+MAMBI_WOE=
+case_when( 
+is.numeric(!!MAMBI)==FALSE ~ NA_real_, 
+!!MAMBI < 0.20 ~3,		
+!!MAMBI >=0.20 & !!MAMBI < 0.39 ~ 2,	
+!!MAMBI >=0.39 & !!MAMBI < 0.53	~1,
+!!MAMBI >=0.53 ~0
+),
+CB_Weight=ifelse(str_detect(!!Basin,"VA Coastal Bays"),0,2),
+MAIA_Weight=ifelse(str_detect(!!Basin,"VA Coastal Bays"),2,1),
+EMAP_Weight=1,
+MAMBI_Weight=1,
+Matrix_score=sum((CB_WOE*CB_Weight),(MAIA_WOE*MAIA_Weight),
+                 (EMAP_WOE*EMAP_Weight),(MAMBI_WOE*MAMBI_Weight))/ sum(CB_Weight,MAIA_Weight,EMAP_Weight,MAMBI_Weight)
+)
+
+}
+
+
+
+
+
+
+
+#=====================================================================================
 
 
 #____________________Just for toxicity salinities____________________
--- Sediment_Chemistry/file_load.R
+++ Sediment_Chemistry/file_load.R
@@ -43,6 +43,180 @@ Total_PCBs=PCBs_CAS$CAS
 
 Sed_Chem_2017_18_19 = read_csv("Sed_Chem_2017_18_19.csv")
 
+
+
+#====================== cleaning BUg data ==================================
+
+# Need to do some string matching of ODU's "Station names" to Dons CBP_Names (i.e with letters added)
+stations_fix=CEDS_Stations[ ,c(1:2)]
+
+str_right =function(string, n) {
+  substr(string, nchar(string) - (n - 1), nchar(string))
+}
+# ===================2019 bugs===============================
+
+# EMAP and MAIA
+
+NCA_MAIA_EMAP2019 =  read_excel("~/RStudio_Test/EMAP_MAIA/NCA_MAIA_EMAP2019.xlsx", 
+                                                    sheet = "NCA_MAIA_EMAP2019")%>%
+dplyr::select(STATION,`DEQ Stion ID`,STATUS,MAIA_INDEX,MAIA_Status=STATUS,EMAP_INDEX,
+              EMAP_STATUS,TOTAL_IND,TOTAL_SPECIES,PIELOUS_EVENNESS_REP,TUBIFICIDAE,SPIONIDAE)%>%
+  rename(Fdt_Sta_Id=`DEQ Stion ID` ) %>%
+  group_by(Fdt_Sta_Id)%>%
+  add_tally()%>%
+  mutate(duplicates=make.unique(STATION,sep="-S"),
+         Ana_Sam_Mrs_Container_Id_Desc=
+           case_when(
+             n==2 & str_detect(duplicates,"-S1")~"S1",
+             n==2 & str_detect(duplicates,"-S1")==FALSE ~"S2",
+             n==1 ~"R"
+           ))   %>%
+  dplyr::select(everything(),-STATION,-n,-duplicates)
+
+# AMBI and MAMBI 
+AMBI_ProbMon_2019= read_excel("~/RStudio_Test/AMBI/AMBI_ProbMon_2019.xlsx") %>%
+  rename(STATIONS=`Stations( & Rep)`)%>%
+  filter(!str_detect(STATIONS,"PR"))%>%
+  separate(STATIONS, c("A", "B","C"),"-") %>% 
+  mutate(STATION=paste0(A,"-",B)) %>%
+ stringdist_left_join(stations_fix,by=c("STATION"="CBP_NAME"), max_dist = 1)%>%
+distinct(STATION,C,AMBI,.keep_all = T)%>%
+  group_by(STATION) %>%
+  add_tally()%>%
+  mutate(duplicates=make.unique(STATION,sep="-S"),
+         Ana_Sam_Mrs_Container_Id_Desc=
+           case_when(
+             n==2 & str_detect(duplicates,"-S1")~"S1",
+             n==2 & str_detect(duplicates,"-S1")==FALSE ~"S2",
+             n==1 ~"R"
+           )) %>%
+  dplyr::select(Fdt_Sta_Id,CBP_NAME,Ana_Sam_Mrs_Container_Id_Desc,everything(),-A,-B,-C,-n,-duplicates)%>%
+  left_join(NCA_MAIA_EMAP2019,by=c("Fdt_Sta_Id","Ana_Sam_Mrs_Container_Id_Desc"))
+
+
+           
+#Individuals   =  or TOTAL_ABUND
+#Taxa          = N_SP_REP_EPI or TOTAL_SPECIES
+#Shannon H'   
+#Gleason D   
+#Pielou Eveness   
+#Tubificidae   
+#Spionidae   
+
+# N_SP_REP_EPI	and TOTAL_ABUND_EPI match Dons numbers
+
+# ches bay bibi
+Bugs_2019_SMH=read_excel("~/RStudio_Test/VARIB/VARIBI5NCA_2019.xlsx") %>%
+group_by(`DEQ Station ID`)%>%
+add_tally() %>%
+mutate(duplicates=make.unique(`DEQ Station ID`,sep="-S"),
+       Ana_Sam_Mrs_Container_Id_Desc=
+case_when(
+  n==2 & str_detect(duplicates,"-S1")~"S1",
+  n==2 & str_detect(duplicates,"-S1")==FALSE ~"S2",
+  n==1 ~"R"
+)) %>%
+mutate(Year="2019")%>%
+dplyr::select(Fdt_Sta_Id=`DEQ Station ID`,Year,B_IBI,Ana_Sam_Mrs_Container_Id_Desc,SHANNON,HABITAT,CBPSEG,BASIN,WATER_BODY  
+              ,STRATUM,-n,-duplicates) %>%
+left_join(AMBI_ProbMon_2019,by=c("Fdt_Sta_Id","Ana_Sam_Mrs_Container_Id_Desc")) %>%
+bug_scores_fun(Basin=BASIN,CB=B_IBI,MAIA=MAIA_INDEX,EMAP=EMAP_INDEX,MAMBI=`M-AMBI`)
+
+x,CB,MAIA,EMAP,MAMBI
+# EMAP & MAIA 
+
+
+
+
+
+# ===================  2018 bugs  ===============================
+
+# Ches bay Bibi
+VARIB_2018_f = read_excel("~/RStudio_Test/VARIB/VARIBI5NCA_2018.xlsx") %>%
+stringdist_left_join(eighteen,by=c("STATION"="CBP_NAME"), max_dist = 1) %>%
+distinct(CBP_NAME,DATE,REP_NUM,B_IBI,.keep_all = T)%>%
+  group_by(Fdt_Sta_Id)%>%
+  add_tally() %>%
+  #dplyr::select(`DEQ Station ID`,n) %>%
+  mutate(duplicates=make.unique(Fdt_Sta_Id,sep="-S"),
+         Ana_Sam_Mrs_Container_Id_Desc=
+           case_when(
+             n==2 & str_detect(duplicates,"-S1")~"S1",
+             n==2 & str_detect(duplicates,"-S1")==FALSE ~"S2",
+             n==1 ~"R"
+           )) %>%
+  mutate(Year="2018",CB_weight=ifelse(CBPSEG=="CSTBY",0,2))%>%
+  dplyr::select(CBP_NAME,Fdt_Sta_Id,Year,CB_IBI=B_IBI,CB_weight,Ana_Sam_Mrs_Container_Id_Desc,SHANNON,HABITAT,CBPSEG,BASIN,WATER_BODY  
+                ,STRATUM,-n,-duplicates) 
+
+
+# ============== 2017 bugs=========================================
+  
+# Ches bay BIBI
+VARIB_2017_f = read_excel("~/RStudio_Test/VARIB/VABIBINCCA_&POT_2017.xls") %>%
+stringdist_left_join(stations_fix %>% filter(str_detect(CBP_NAME,"VA17")),by=c("STATION"="CBP_NAME"), max_dist = 1) %>%
+filter(!str_detect(STATION,"PR"))%>%
+distinct(CBP_NAME,DATE,REP_NUM,B_IBI,.keep_all = T)%>%
+  group_by(Fdt_Sta_Id)%>%
+  add_tally() %>%
+  #dplyr::select(`DEQ Station ID`,n) %>%
+  mutate(duplicates=make.unique(Fdt_Sta_Id,sep="-S"),
+         Ana_Sam_Mrs_Container_Id_Desc=
+           case_when(
+             n==2 & str_detect(duplicates,"-S1")~"S1",
+             n==2 & str_detect(duplicates,"-S1")==FALSE ~"S2",
+             n==1 ~"R"
+           )) %>%
+  mutate(Year="2017",CB_weight=ifelse(CBPSEG=="CBAYS",0,2))%>%
+  dplyr::select(CBP_NAME,Fdt_Sta_Id,Year,CB_IBI=B_IBI,CB_weight,Ana_Sam_Mrs_Container_Id_Desc,SHANNON,HABITAT,CBPSEG,BASIN,WATER_BODY  
+                ,STRATUM,-n,-duplicates) 
+
+
+
+
+#========================== 2016 bugs=====================================
+
+sixteen=
+  stations_fix %>% 
+  filter(str_detect(CBP_NAME,"VA16")) %>% 
+  mutate(letters=str_right(CBP_NAME,1))
+
+#CB bibi
+VARIB_2016_f = read_excel("~/RStudio_Test/VARIB/VARIBI5NCA_2016ModifiedMediomastus.XLS", 
+                             skip = 1)%>%
+  mutate(STATION=str_remove(STATION, "0"))%>%
+  stringdist_left_join(sixteen,by=c("STATION"="CBP_NAME"), max_dist = 1) %>%
+  distinct(STATION,DATE,REP_NUM,B_IBI,.keep_all = T)%>%
+  group_by(Fdt_Sta_Id)%>%
+  add_tally() %>%
+  #dplyr::select(`DEQ Station ID`,n) %>%
+  mutate(duplicates=make.unique(Fdt_Sta_Id,sep="-S"),
+         Ana_Sam_Mrs_Container_Id_Desc=
+           case_when(
+             n==2 & str_detect(duplicates,"-S1")~"S1",
+             n==2 & str_detect(duplicates,"-S1")==FALSE ~"S2",
+             n==1 ~"R"
+           )) %>%
+  mutate(Year="2016",CB_weight=ifelse(CBPSEG=="CSTBY",0,2))%>%
+  dplyr::select(CBP_NAME,Fdt_Sta_Id,Year,CB_IBI=B_IBI,CB_weight,Ana_Sam_Mrs_Container_Id_Desc,SHANNON,HABITAT,CBPSEG,BASIN,WATER_BODY  
+                ,STRATUM,-n,-duplicates) 
+
+
+#=======================================================================================
+# VARIB_2015 =read_excel("~/RStudio_Test/VARIB/VARIBI5NCA_2015.XLS")
+
+
+
+CB_IBI_2016_2019=rbind(VARIB_2016_f,VARIB_2017_f,VARIB_2018_f,VARIB_2019_f)
+
+
+
+#=======================================================================================
+
+
+
+
+
 # check names any(!names(EDD_2017) %in% c(names(EDD_2018),names(EDD_2019))
 
 
